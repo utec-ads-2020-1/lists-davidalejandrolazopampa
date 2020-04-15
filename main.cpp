48c6f2322a4e12c422330b4dc14deca9ef56901c
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2020-04-15 15:53:32 -0500

    second Prueba Stack size

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include "test/tester.h"
-
+//#include "test/tester.h"
+#include "structures/stack.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
     cout << "\tLists Practice" << endl;
     cout << "===========================================================" << endl << endl;
+    auto *stack2 = new stack<int>();
 
-    Tester::execute();
+    //Tester::execute();
     return EXIT_SUCCESS;
 }
