26b47d7b10aaf8f1d1fbc9a9e9d895e380308c7c
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2020-04-15 15:52:31 -0500

    second Forward

--- structures/lists/iterators/forward_iterator.h
+++ structures/lists/iterators/forward_iterator.h
@@ -21,33 +21,37 @@ class ForwardIterator {
 
         T operator*();
 
-        //////////Iniciando//////////
-        ForwardIterator(){
 
-        }
-        ForwardIterator(Node<T>*){
-
-        }
-        /*Equals an iterator to another*/
-        ForwardIterator<T> operator=(ForwardIterator<T>){
-
-        }
-        /*Returns if the iterator is different from another iterator*/
-        bool operator!=(ForwardIterator<T>){
-
-        }
-        /*Advance one position*/
-        ForwardIterator<T> operator++(){
-
-        }
-        /*Goes back one position*/
-        ForwardIterator<T> operator--(){
-
-        }
-        /*Returns the content of the iterator*/
-        T operator*(){
-
-        }
 };
+//////////Iniciando//////////
+template <typename T>
+ForwardIterator<T>::ForwardIterator(){
 
+}
+template <typename T>
+ForwardIterator<T>::ForwardIterator(Node<T> *) {
+
+}
+/*Equals an iterator to another*/
+template <typename T>
+ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T>) {
+
+}
+
+/*Returns if the iterator is different from another iterator*/
+template <typename T>
+bool ForwardIterator<T>::operator!=(ForwardIterator<T>){
+
+}
+/*Advance one position*/
+template <typename T>
+ForwardIterator<T> ForwardIterator<T>::operator++() {
+
+}
+
+/*Returns the content of the iterator*/
+template <typename T>
+T ForwardIterator<T>::operator*() {
+
+}
 #endif
